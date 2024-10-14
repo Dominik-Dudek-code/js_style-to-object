aa7e4513247308904633ba1634bee81f246fe95f
Dominik Dudek <dom-dud774@o2.pl>
2024-10-14 23:48:18 +0200

    Task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(/[;:]/)
+    .map((trimmedString) => trimmedString.trim())
+    .filter((nonEmptyString) => nonEmptyString.length > 0)
+    .reduce((resultObject, currentString, stringIndex, allStrings) => {
+      if (stringIndex % 2 === 0) {
+        resultObject[currentString] = allStrings[stringIndex + 1];
+      }
+
+      return resultObject;
+    }, {});
 }
 
 module.exports = convertToObject;
